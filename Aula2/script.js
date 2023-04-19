452327f733d926f7346eb4013d619222321ee5d6
marianaozorio <mariana.athayde@gmail.com>
2023-04-18 22:10:57 -0300

    css + js class

---
+++ Aula2/script.js
@@ -0,0 +1,12 @@
+console.log("Hello, World");
+
+let nome = "Mariana";
+
+let pessoa = {
+    nome: 'Mari',
+    idade: 30,
+    endereco: "Av. Paulista, 222"
+};
+
+console.log(pessoa.endereco);
+console.log("Hello, " + pessoa.nome);
