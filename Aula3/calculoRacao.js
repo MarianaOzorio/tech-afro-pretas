84739575e01abbbe8c975f008c32af392f599e54
marianaozorio <mariana.athayde@gmail.com>
2023-04-22 15:18:28 -0300

    exercise class 4

--- Aula3/calculoRacao.js
+++ Aula3/calculoRacao.js
@@ -1,8 +1,15 @@
+// exercício aula 4: calcular 1% do peso do cachorro para a quantidade de ração
+
 let btn = document.getElementById("calcular");
 let resultadoTxt = document.getElementById("resultado");
 
 btn.addEventListener("click", calcularResultado);
 
 function calcularResultado() {
-    resultadoTxt.innerHTML = "Aqui vai aparecer o resultado"
+    let nomePet = document.getElementById("nome").value;
+    let pesoPet = document.getElementById("number").value;
+
+    let totaRacao = parseInt(pesoPet*1)/100*1000;
+
+    resultadoTxt.innerHTML = `${nomePet} deve comer ${totaRacao}g de ração por dia.`
 };
