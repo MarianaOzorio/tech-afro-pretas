295d9c5772a9929febff43cc52f896dde4a326b5
marianaozorio <mariana.athayde@gmail.com>
2023-04-29 14:36:16 -0300

    create component

--- books/src/Components/Header/Header.jsx
+++ books/src/Components/Header/Header.jsx
@@ -1,8 +1,25 @@
 import './Header.css';
 
-function Header(){
+import CardInfo from '../CardInfo/CardInfo';
+
+function Header(user){
     return(
-        <div></div>
+        <header>
+            <div className='photoName'>
+                <img src={user.photo} alt="foto da pessoa usuária"/>
+                <p>{user.name}</p>
+            </div>
+            <div className='infoCards'>
+                <CardInfo
+                    number='130'
+                    txt='lidos'
+                />
+                <CardInfo
+                    number='60'
+                    txt='vou ler'
+                />
+            </div>
+        </header>
     )
 };
 
