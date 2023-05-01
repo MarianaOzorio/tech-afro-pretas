f3a4da230337953ce64b45646b8371ce0182cf5c
marianaozorio <mariana.athayde@gmail.com>
2023-05-01 18:38:14 -0300

    add buttons

--- books/src/Components/Menu/Menu.jsx
+++ books/src/Components/Menu/Menu.jsx
@@ -1,8 +1,29 @@
 import './Menu.css';
 
+import Button from '../Button/Button';
+
+import { AiOutlineHeart, AiOutlineGift, AiOutlineStar, AiOutlineRead } from 'react-icons/ai';
+
 function Menu(){
     return(
-        <div></div>
+        <div className='menu'>
+            <Button
+                icon={ <AiOutlineHeart /> }
+                txt='Favoritos'
+            /> 
+            <Button
+                icon={ <AiOutlineGift /> }
+                txt='Desejados'
+            /> 
+            <Button
+                icon={ <AiOutlineStar /> }
+                txt='Avaliados'
+            /> 
+            <Button
+                icon={ <AiOutlineRead /> }
+                txt='Resenhas'
+            />    
+        </div>
     )
 };
 
