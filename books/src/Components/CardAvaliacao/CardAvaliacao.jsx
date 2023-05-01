40ddac725e5aaa79d5a964996ee47f9fc1fc1598
marianaozorio <mariana.athayde@gmail.com>
2023-05-01 18:57:28 -0300

    add content

--- books/src/Components/CardAvaliacao/CardAvaliacao.jsx
+++ books/src/Components/CardAvaliacao/CardAvaliacao.jsx
@@ -1,8 +1,19 @@
+import Avaliacao from '../Avaliacao/Avaliacao';
+
 import './CardAvaliacao.css';
 
-function CardAvaliacao(){
+function CardAvaliacao(review){
     return(
-        <div></div>
+        <div className='cardReview'>
+            <div className='cardReviewImg'>
+                <img src={review.cover} alt="capa do livro" />
+                <Avaliacao />
+            </div>
+            <div>
+                <p className='titleReview'>{review.title}</p>
+                <p>{review.txt}</p>
+            </div>
+        </div>
     )
 };
 
