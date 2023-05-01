df31dcafe902ae83cbc2dc55baa3307cb3100045
marianaozorio <mariana.athayde@gmail.com>
2023-05-01 12:38:22 -0300

    change header style

--- books/src/Components/Header/Header.jsx
+++ books/src/Components/Header/Header.jsx
@@ -7,11 +7,11 @@ function Header(user){
         <header>
             <div className='photoName'>
                 <img src={user.photo} alt="foto da pessoa usuária"/>
-                <p>{user.name}</p>
+                <h3>{user.name}</h3>
             </div>
             <div className='infoCards'>
                 <CardInfo
-                    number='130'
+                    number='70'
                     txt='lidos'
                 />
                 <CardInfo
