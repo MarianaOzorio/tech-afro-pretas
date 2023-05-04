5e0ded1ba616beec08935679b60fe0956bad5e8d
marianaozorio <mariana.athayde@gmail.com>
2023-05-03 22:28:50 -0300

    useState activity

--- books/src/Pages/Favoritos/Favoritos.jsx
+++ books/src/Pages/Favoritos/Favoritos.jsx
@@ -1,6 +1,43 @@
+import React, { useState } from "react";
+import './Favoritos.css';
+
 function Favoritos(){
+
+    const [classe, setClasse] = useState('light');
+    const [titulo, setTitulo] = useState('Hello');
+    const [favorito, setFavorito] = useState(0);
+
+    function handleClickDarkMode(){
+        if (classe === "dark")
+            setClasse("light")
+        else
+            setClasse("dark")  
+    };
+
+    function handleChangeTitle(){
+        if (titulo === "Hello")
+            setTitulo("Favoritos")
+        else
+            setTitulo("Hello")  
+    };
+
+    function handleAdFav(){
+        setFavorito(favorito+1)  
+    };
+
+    function handleRemoveFav(){
+        setFavorito(favorito-1)  
+    };
+
     return(
-        <h1>Favoritos</h1>
+        <div className={classe}>
+            <h2>{titulo}</h2>
+            <button onClick={handleClickDarkMode}>Mudar fundo</button>
+            <button onClick={handleChangeTitle}>Mudar título</button>
+            <p> Tenho {favorito} livros favoritos</p>
+            <button onClick={handleAdFav}>Adicionar favorito</button>
+            <button onClick={handleRemoveFav}>Remover favorito</button>
+        </div>
     )
 }
 
